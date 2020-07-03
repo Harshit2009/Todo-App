a077e19cdbc0f77704a07aa665fbbec861e8b52a
Rudrgoel <harshit18csu078@ncuindia.edu>
2020-07-04 00:09:09 +0530

    Adding Files

---
+++ todos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+# Create your models here.
+class Todo(models.Model):
+    todo = models.CharField(max_length=100, null=False,
+                            help_text="This field is required")
+    done = models.BooleanField(default=False)

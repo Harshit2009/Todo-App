8a1088ec73464cb2c07b9b5a651e65b990af981a
Harshit Goel <64546139+Harshit2009@users.noreply.github.com>
2020-07-05 21:20:33 +0530

    Update settings.py

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jcz4cym39)0kalke&4au*rh@@#^c(=7)!!(%+_m$cwc(&ksp$m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
